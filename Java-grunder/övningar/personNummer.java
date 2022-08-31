35620b92bc70d7de58711b84537432c9af5492d2
braindome <104855075+braindome@users.noreply.github.com>
2022-08-31 02:56:39 +0200

    Create personNummer.java

---
+++ Java-grunder/övningar/personNummer.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Uppgift 3:
+        //Skriv ett program som kollar om inmatad sträng är ett giltigt personnummer.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ange personnummer (12 siffror):");
+        String pnInput = sc.nextLine();
+
+        //Skapar en stränglista med enskilda siffror
+        String[] numbers = pnInput.split("");
+        System.out.println(Arrays.toString(numbers));
+
+        //Skapar en tom integerlista
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        //Parsear stränglistan och lägger till integer till en ny integerlistan
+        for (int i = 0; i < numbers.length-1; i++) {
+            intList.add(parseInt(numbers[i]));
+        }
+
+        //Luhn-algoritmen
+        int sum = intList.get(2) *2 + intList.get(3) + intList.get(4) *2 + intList.get(5) + intList.get(6) *2 + intList.get(7) + intList.get(8) *2 + intList.get(9);
+        if (sum % 10 == 0) {
+            System.out.println("Personnumret är giltigt");
+        } else {
+            System.out.println("Personnumret är inte giltigt");
+        }
+
+
+        //UNDER OMBYGGNAD
+        /*int k = 0;
+        ArrayList<Integer> evens = new ArrayList<>();
+        ArrayList<Integer> odds = new ArrayList<>();
+        while ( k <= numbers.length) {
+            for (int i = 0; i % 2 == 0; i++) {
+                evens.add(intList.get(i) + intList.get(i));
+                System.out.println(evens);
+            }
+
+            for (int i = 0; i % 2 != 0; i++) {
+                odds.add(intList.get(i));
+            }
+            k++;
+
+        }*/
+
+    }
+}
