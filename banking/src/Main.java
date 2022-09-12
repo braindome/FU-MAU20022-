9a724098595405df23e3af01d0f7362ceb82fa78
Antonio Saracino <104855075+braindome@users.noreply.github.com>
2022-09-12 11:26:27 +0200

    Banking exercise

---
+++ banking/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Account acct1 = new Account();
+    acct1.inputBalance(20000.00);
+    acct1.setName("Antonio Saracino");
+    acct1.toString();
+    }
+}
